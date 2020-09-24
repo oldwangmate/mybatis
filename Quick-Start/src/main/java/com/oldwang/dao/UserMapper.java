d6e57c0d64af0790c35c54b6bf55ea5fb059b770
oldwangmate <wanghengmate@163.com>
2020-09-24 22:46:10 +0800

    CURD

--- Quick-Start/src/main/java/com/oldwang/dao/UserMapper.java
+++ Quick-Start/src/main/java/com/oldwang/dao/UserMapper.java
@@ -3,8 +3,28 @@ package com.oldwang.dao;
 import com.oldwang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
+    //查询全部用户
     List<User> getUserList();
+
+    //根据ID查询用户
+    User getUserById(int id);
+
+    //添加用户
+    int addUser(User user);
+
+    //修改用户
+    int updateUser(User user);
+
+    //删除用户
+    int deleteUser(int id);
+
+    //map插入用户
+    int addUserByMap(Map<String,Object> map);
+
+    //模糊查询
+    List<User> getUserByLike(String userName);
 }
--- Quick-Start/src/test/java/com/oldwang/dao/UserMapperTest.java
+++ Quick-Start/src/test/java/com/oldwang/dao/UserMapperTest.java
@@ -5,12 +5,14 @@ import com.oldwang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
 
     @Test
-    public void test(){
+    public void getUserList(){
         //获取SQLSession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         //获取UserMapper对象 方式一
@@ -22,6 +24,77 @@ public class UserMapperTest {
         List<User> users = sqlSession.selectList("com.oldwang.dao.UserMapper.getUserList", User.class);
         users.forEach(System.out::println);
         //释放资源
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void getUserById(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User userById = mapper.getUserById(1);
+        System.out.println(userById);
+        sqlSession.close();
+    }
+
+    @Test  //增删改需要提交事务
+    public void addUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(4);
+        user.setName("张三");
+        user.setPwd("123");
+        mapper.addUser(user);
+        //提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(4);
+        user.setName("张三");
+        user.setPwd("123345");
+        mapper.updateUser(user);
+        //提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.deleteUser(4);
+        //提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUserByMap(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",4);
+        map.put("name","张三");
+        map.put("pwd","123456");
+        mapper.addUserByMap(map);
+        //提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserByLike(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userByLike = mapper.getUserByLike("test");
+        userByLike.forEach(System.out::println);
         sqlSession.close();
     }
 }
