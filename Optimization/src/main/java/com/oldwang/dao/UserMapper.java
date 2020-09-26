d625711f96f2cfeba4bbe0cfe214e3193920db60
oldwangmate <wanghengmate@163.com>
2020-09-26 10:10:48 +0800

    Configuration  AND Log4j

---
+++ Optimization/src/main/java/com/oldwang/dao/UserMapper.java
@@ -0,0 +1,37 @@
+package com.oldwang.dao;
+
+import com.oldwang.pojo.User;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+
+    //查询全部用户
+    List<User> getUserList();
+
+    //根据ID查询用户
+    User getUserById(int id);
+
+    //添加用户
+    int addUser(User user);
+
+    //修改用户
+    int updateUser(User user);
+
+    //删除用户
+    int deleteUser(int id);
+
+    //map插入用户
+    int addUserByMap(Map<String, Object> map);
+
+    //模糊查询
+    List<User> getUserByLike(String userName);
+
+    //分页查询
+    List<User> getLimitUser(Map<String,Integer> map);
+
+    //分页查询 RowBounds
+    List<User> getUserByRowBounds(RowBounds rowBounds);
+}
